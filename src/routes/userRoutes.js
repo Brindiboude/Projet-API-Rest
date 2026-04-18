b45ca08765d638fe5c9b0b3a6d20817ee374ceae
Brindiboude <abbas.sarah2006@gmail.com>
2026-04-18 20:15:25 +0200

    feat: protect all user routes with authenticate

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,9 +5,9 @@ import authenticate from "../middlewares/authenticate.js";
 const router = Router();
 
 router.get("/", authenticate, userController.getAllUsers);
-router.get("/:id", userController.getUserById);
-router.post("/", userController.createUser);
-router.put("/:id", userController.updateUser);
+router.get("/:id", authenticate, userController.getUserById);
+router.post("/", authenticate, userController.createUser);
+router.put("/:id", authenticate, userController.updateUser);
 router.delete("/:id", authenticate, userController.deleteUser);
 
 export default router;
